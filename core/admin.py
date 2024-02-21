bb23a008a3d729cdef25c402c727a76083e48438
harisABCInd <haris@abcindllc.com>
2024-02-21 15:32:13 +0400

    django admin added & user fixes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from core.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('first_name', 'last_name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')
+        }),
+    )
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
